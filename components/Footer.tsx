b62a559abd505fe61613fa14b45f25fec24563da
Alexandre Caramel <caramel.alexandre@gmail.com>
2021-10-26 15:44:04 +0200

    push

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>Next js app powered by @terminev</p>
+        </footer>
+    )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+export default function Header() {
+    return (
+        <header>
+            <Link href="/">
+                <img src="iphone.png" alt="img iphone" />
+            </Link>
+            <Link href="/">
+                <h1>Iphone Shop</h1>
+            </Link>
+
+            <a href="#" style={{textDecoration: "none"}}>
+                <svg width="31" height="27" viewBox="0 0 31 27" fill="none" xmlns="<http://www.w3.org/2000/svg>">
+                    <path d="" fill="#9094FF"/>
+                </svg>
+                <span></span>
+            </a>
+        </header>
+    )
+}
